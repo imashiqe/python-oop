e332eba9f91ab6a171abc12067b2004efb885688
Ashiqur Rahman <imashiqe@gmail.com>
2024-11-18 01:01:38 +0600

    Input  Output and Typecasting in python

---
+++ basic day1/input.py
@@ -0,0 +1,28 @@
+""" print('Now I need money')
+
+input('Enter your name: ') """
+
+#money = input('Enter your money: ')
+#print(money)
+
+#print("here is your money: ", money)
+
+first_money = input('Enter your money rofiq: ')
+second_money = input('Enter your money karim: ')
+
+first_moneyint = int(first_money)
+second_moneyint = int(second_money)
+
+#print(first_money , second_money)
+
+total_money = ( first_moneyint +  second_moneyint)
+
+print('Total money is: ', total_money)
+
+""" google search: python input function
+https://www.w3schools.com/python/ref_func_input.asp
+1. convert number to  string: str()
+2. convert decimal to number: int()
+3. convert number to decimal: float()
+
+"""
