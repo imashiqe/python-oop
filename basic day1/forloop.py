497c0d6f0de221507f291a65b8410769644d482e
Ashiqur Rahman <imashiqe@gmail.com>
2024-11-18 14:14:24 +0600

    for loop in python and range in python

---
+++ basic day1/forloop.py
@@ -0,0 +1,15 @@
+numbers = [10, 20, 30, 40, 50]
+sum = 0
+for num in numbers:
+    print(num)
+    sum = sum + num
+    if sum > 20:
+        print('bigger sum')
+print(sum)
+
+""" text = 'Pagla Howar'
+for char in text:
+    print(char) """
+
+for i in range(1,10,2):
+    print(i)
