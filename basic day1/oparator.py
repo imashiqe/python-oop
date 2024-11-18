d12b95cdd6e63301a357d3a36515a9c4f57261d4
Ashiqur Rahman <imashiqe@gmail.com>
2024-11-18 12:20:24 +0600

    Operators

---
+++ basic day1/oparator.py
@@ -0,0 +1,13 @@
+#+,-,*,/
+
+num1 = 45
+num2 = 5
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+
+print(10%3) # reminder
+print(10//3) # floor division
+print(10**3) # power
