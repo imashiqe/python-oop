6da9727a958528fb10ccc4bb00d33d8acbf35a73
Ashiqur Rahman <imashiqe@gmail.com>
2024-11-18 16:43:07 +0600

    Introduction to functions in python

--- basic day1/summary.py
+++ basic day1/summary.py
@@ -16,3 +16,20 @@ Homework
 4. run a loop and show my the odd numbers between 39 to 68
 
  """
+
+""" x = 5
+y = 3
+print(x % y) """
+
+""" x = 7
+if x > 5:
+    print("Greater than 5")
+else:
+    print("Less than or equal to 5") """
+
+# for i in range(5):
+#     print(i)
+
+x = 10
+y = 2
+print(x // y)
