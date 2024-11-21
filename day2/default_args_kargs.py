2fc7591045bde138c862ad5cc898c5fc6c62b199
Ashiqur Rahman <imashiqe@gmail.com>
2024-11-21 16:26:40 +0600

    Default parameters and args in python

---
+++ day2/default_args_kargs.py
@@ -0,0 +1,23 @@
+def sum(sum1,sum2):
+    result = sum1 + sum2
+    return result
+
+total = sum(44,39)
+
+print('total value', total)
+
+#args 
+
+def all_sum(num1,num2*numbers):
+    print(numbers)
+    sum= 0
+    for num in numbers:
+        print(num)
+        sum = sum + sum
+     return sum
+total = all_sum(45,56,89,11,46)
+print('all sum', total)
+
+def dp_a_lot(*args):
+    print(args)
+ 
