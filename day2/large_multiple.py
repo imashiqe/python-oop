fc127998bc76c5da0173d398502abd7e99981605
Ashiqur Rahman <imashiqe@gmail.com>
2024-12-20 16:08:52 +0600

    (advanced) kargs, multiple return from a function

--- day2/large_multiple.py
+++ day2/large_multiple.py
@@ -6,3 +6,11 @@ def full_name(first, last):
 name = full_name('John', 'Doe')
 
 print(name)
+
+def famous_name(first,last,title,**addition):
+    name = f'{title=} {first} {last} {addition}'
+    return name
+
+    name = famous_name(first='John', last='Doe', title='Mr.', addition='Jr.')
+
+    print(name)
